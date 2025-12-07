72eb59024ad9d91bc183a4e1411f6e48543688a4
Alexander Sergeev <etc9053@gmail.com>
2025-12-07 22:46:18 +0300

    add taplo

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -11,5 +11,14 @@
       enable = true;
       settings.formatter.retain_line_breaks = true;
     };
+    # TOML
+    taplo = {
+      enable = true;
+      settings = {
+        reorder_keys = true;
+        reorder_arrays = true;
+        reorder_inline_tables = true;
+      };
+    };
   };
 }
